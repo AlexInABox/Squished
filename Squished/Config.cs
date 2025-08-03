9a74ee986aa1d819498dca866700e61718fea7f2
Alexander Betke <main@alexinabox.de>
2025-08-03 22:29:41 +0200

    chore: update to newest LabAPI

--- Squished/Config.cs
+++ Squished/Config.cs
@@ -1,12 +1,9 @@
-using System.ComponentModel;
-using UnityEngine;
-
 namespace Squished;
 
 public class Config
 {
     public bool Debug { get; set; } = false;
-    
+
     public string BonkSoundEffectPath { get; set; } =
         "/home/container/.config/SCP Secret Laboratory/LabAPI/configs/7100/Squished/bonk.ogg";
 }
--- Squished/EventHandlers.cs
+++ Squished/EventHandlers.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
 using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Events.Handlers;
 using LabApi.Features.Wrappers;
-using LabApi.Features.Console;
 using PlayerStatsSystem;
 using UnityEngine;
 using Logger = LabApi.Features.Console.Logger;
@@ -25,49 +23,42 @@ public static class EventHandlers
     private static void OnHurting(PlayerHurtingEventArgs ev)
     {
         float fallDamageDealt = FallDamage(ev.DamageHandler);
-        if (fallDamageDealt <= 0f)
-        {
-            return;
-        }
+        if (fallDamageDealt <= 0f) return;
 
         int bonkCount = 0;
         Collider[] hitColliders = Physics.OverlapSphere(ev.Player.Position, 1f);
         foreach (Collider collider in hitColliders)
-        {
             if (Player.TryGet(collider.gameObject, out Player player))
             {
-                if(player.PlayerId == ev.Player.PlayerId || player.IsGodModeEnabled) continue;
-                DamageHandlerBase damageHandler = new CustomReasonDamageHandler("Zerquetscht von " + ev.Player.Nickname + "!", fallDamageDealt, "");
+                if (player.PlayerId == ev.Player.PlayerId || player.IsGodModeEnabled) continue;
+                DamageHandlerBase damageHandler =
+                    new CustomReasonDamageHandler("Zerquetscht von " + ev.Player.Nickname + "!", fallDamageDealt);
                 player.Damage(damageHandler);
                 bonkCount++;
             }
-        }
-        
+
         PlaySoundEffect(ev.Player.Position, bonkCount);
     }
-    
+
     private static float FallDamage(DamageHandlerBase dHandler)
     {
         if (dHandler is UniversalDamageHandler uHandler)
         {
             Logger.Debug(uHandler.ServerLogsText);
-            if (uHandler.ServerLogsText.Contains("Fall"))
-            {
-                return uHandler.Damage;
-            }
+            if (uHandler.ServerLogsText.Contains("Fall")) return uHandler.Damage;
         }
 
         return 0f;
     }
-    
+
     private static void PlaySoundEffect(Vector3 pos, int bonkCount)
     {
         if (bonkCount <= 0) return;
-        AudioPlayer audioPlayer = AudioPlayer.CreateOrGet($"squished_audioplayer" + pos.GetHashCode());
+        AudioPlayer audioPlayer = AudioPlayer.CreateOrGet("squished_audioplayer" + pos.GetHashCode());
         audioPlayer.AddSpeaker("squished_speaker" + pos.GetHashCode(), pos, 5F + bonkCount, true, 5F, 1000F);
         audioPlayer.DestroyWhenAllClipsPlayed = true;
         audioPlayer.AddClip("squished_sound_effect", 5F + bonkCount);
-        
+
         Logger.Debug("Playing sound effect at position: " + pos);
     }
 }
